e7f71b69f56453b288a73ad4235608a4b7d28144
OmgRod <89850217+OmgRod@users.noreply.github.com>
2026-03-23 22:05:38 +0000

    update next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
+const fs = require('node:fs');
+
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repository = process.env.GITHUB_REPOSITORY || '';
+const repoName = repository.split('/')[1] || '';
+const hasCustomDomain = fs.existsSync('./CNAME');
+
+const repoBasePath = !hasCustomDomain && repoName ? `/${repoName}` : '';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? repoBasePath : '',
+  assetPrefix: isGithubActions && repoBasePath ? `${repoBasePath}/` : undefined,
 };
 
 module.exports = nextConfig;
