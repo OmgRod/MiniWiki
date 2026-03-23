28e2cd749ccca60af39d225ee73dfdd96febf2ed
OmgRod <89850217+OmgRod@users.noreply.github.com>
2026-03-23 22:43:59 +0000

    more components and new components docs :)

--- components/mdx-components.js
+++ components/mdx-components.js
@@ -6,12 +6,21 @@ import Callout from './Callout';
 import Card from './Card';
 import CardGrid from './CardGrid';
 import CodeBlock from './CodeBlock';
+import DarkModeToggle from './DarkModeToggle';
 import Divider from './Divider';
+import DefinitionList from './DefinitionList';
 import FeatureList from './FeatureList';
+import Figure from './Figure';
+import Footer from './Footer';
+import Header from './Header';
+import Infobox from './Infobox';
 import Icon from './Icon';
 import Kbd from './Kbd';
+import SearchBar from './SearchBar';
+import Sidebar from './Sidebar';
 import Steps from './Steps';
 import Tabs, { TabPanels } from './Tabs';
+import Table, { Caption, TBody, TD, TH, THead, TR } from './WikiTable';
 
 const mdxComponents = {
   Accordion,
@@ -21,15 +30,37 @@ const mdxComponents = {
   Callout,
   Card,
   CardGrid,
+  Caption,
   CodeBlock,
+  DarkModeToggle,
+  DefinitionList,
   Divider,
   FeatureList,
+  Figure,
+  Footer,
+  Header,
   Icon,
+  Infobox,
   Kbd,
+  SearchBar,
+  Sidebar,
   Steps,
+  Table,
+  TBody,
+  TD,
+  TH,
+  THead,
+  TR,
   Tabs,
   TabPanels,
+  caption: (props) => <Caption {...props} />,
   pre: (props) => <CodeBlock {...props} />,
+  table: (props) => <Table {...props} />,
+  tbody: (props) => <TBody {...props} />,
+  td: (props) => <TD {...props} />,
+  th: (props) => <TH {...props} />,
+  thead: (props) => <THead {...props} />,
+  tr: (props) => <TR {...props} />,
 };
 
 export default mdxComponents;
